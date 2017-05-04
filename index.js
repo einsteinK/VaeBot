8ad060e791129c824c49f8ca6938411d3b3a8664
Vaeb <vaebmail@gmail.com>
2017-05-05 00:23:58 +0100

    Command updates

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ global.Discord = require("discord.js");
 
 const Auth = require("./Auth.js");
 
+Discord.GuildMember.prototype.getProp = function(p) {
+	if (this[p] != null) return this[p];
+	return this.user[p];
+};
+
+Discord.User.prototype.getProp = function(p) {
+	return this[p];
+};
+
 global.client = new Discord.Client({
 	disabledEvents: ["TYPING_START"],
 	fetchAllMembers: true,
@@ -55,13 +64,6 @@ global.colCommand = 0x2196F3; // Log of command being executed
 var blockedUsers = {};
 
 var runFuncs = [];
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-
-Discord.GuildMember.prototype.getProp = function(p) {
-	if (this[p] != null) return this[p];
-	return this.user[p];
-};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -265,30 +267,35 @@ client.on("guildMemberAdd", member => {
 
 	if (guild.id == "309785618932563968") {
 		var veilGuild = client.guilds.get("284746138995785729");
-		if (!veilGuild) return console.log("[ERROR_VP] Veil guild not found!");
-		var veilMember = Util.getMemberById(memberId, veilGuild);
-		if (!veilMember) {
-			console.log("[Auto-Kick 1] User not in Veil");
-			member.kick()
-			.catch(error => console.log("\n[E_AutoKick1] " + error));
-			return;
-		}
 		var veilBuyer = veilGuild.roles.find("name", "Buyer");
-		if (!veilBuyer) return console.log("[ERROR_VP] Veil Buyer role not found!");
-		if (!veilMember.roles.has(veilBuyer.id)) {
-			console.log("[Auto-Kick 2] User does not have Buyer role");
-			member.kick()
-			.catch(error => console.log("\n[E_AutoKick2] " + error));
-			return;
-		}
 		var newBuyer = guild.roles.find("name", "Buyer");
-		if (!newBuyer) return console.log("[ERROR_VP] New Buyer role not found!");
-		member.addRole(newBuyer)
-		.catch(error => console.log("\n[E_AutoAddRole1] " + error));
-		console.log("Awarded new member with Buyer role");
+		if (!veilGuild) {
+			console.log("[ERROR_VP] Veil guild not found!");
+		} else if (!veilBuyer) {
+			console.log("[ERROR_VP] Veil Buyer role not found!");
+		} else if (!newBuyer) {
+			console.log("[ERROR_VP] New Buyer role not found!");
+		} else {
+			var veilMember = Util.getMemberById(memberId, veilGuild);
+			if (!veilMember) {
+				console.log("[Auto-Kick 1] User not in Veil");
+				member.kick()
+				.catch(error => console.log("\n[E_AutoKick1] " + error));
+				return;
+			}
+			if (!veilMember.roles.has(veilBuyer.id)) {
+				console.log("[Auto-Kick 2] User does not have Buyer role");
+				member.kick()
+				.catch(error => console.log("\n[E_AutoKick2] " + error));
+				return;
+			}
+			member.addRole(newBuyer)
+			.catch(error => console.log("\n[E_AutoAddRole1] " + error));
+			console.log("Awarded new member with Buyer role");
+		}
 	}
 
-	if (guild.id == "168742643021512705" || guild.id == "284746138995785729" || guild.id == "166601083584643072") {
+	if (Util.getRole("SendMessages", guild)) {
 		var isMuted = Mutes.checkMuted(memberId, guild);
 
 		if (isMuted == true) {
